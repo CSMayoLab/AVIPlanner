f19381900f7f5ac56b1705d8428499f8855f3982
Yuan Yao <yuanyao.or.john@gmail.com>
2024-10-04 02:20:31 -0400

    Add Script Launcher project

---
+++ ScriptLauncher/ScriptLauncher.cs
@@ -0,0 +1,64 @@
+﻿//NOTICE: © 2022 The Regents of the University of Michigan
+
+//The Chuck Mayo Lab - https://medicine.umich.edu/dept/radonc/research/research-laboratories/physics-laboratories 
+
+//The software is solely for non-commercial, non-clinical research and education use in support of the publication.
+//It is a decision support tool and not a surrogate for professional clinical guidance and oversight.
+//The software calls APIs that are owned by Varian Medical Systems, Inc. (referred to here as Varian),
+//and you should be aware you will need to obtain an API license from Varian in order to be able to use those APIs.
+//Extending the [No Liability] aspect of these terms, you agree that as far as the law allows,
+//Varian and Michigan will not be liable to you for any damages arising out of these terms or the use or nature of the software,
+//under any kind of legal claim, and by using the software, you agree to indemnify the licensor and Varian in the event that the
+//licensor or Varian is joined in any lawsuit alleging injury or death or any other type of damage (e.g., intellectual property infringement)
+//arising out of the dissemination or use of the software.
+
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public void Execute(ScriptContext context)
+        {
+            try
+            {
+                // Just force to use VMS.TPS.Common.Model.Types;
+                VVector xxx = new VVector(); xxx.x = 0.3; 
+                
+                // Dummy reference to workaround ESAPI bug
+                Patient patientxxx = null; if (patientxxx != null) { int i = 3; i = i / 3; };  
+
+                Process.Start(AppExePath());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Failed to start standAlone application: {AppExePath()}\n\nThere can only be a single .exe file in the folder.");
+            }
+        }
+
+        private string AppExePath()
+        {
+            return FirstExePathIn(AssemblyDirectory());
+        }
+
+        private string FirstExePathIn(string dir)
+        {
+            return Directory.GetFiles(dir, "*.exe").Single();
+            
+        }
+
+        private string AssemblyDirectory()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+    }
+}
